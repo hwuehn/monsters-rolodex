6c42c18caa8c56999abb2607e0bc2de08b1ed847
Henning Wuehn <h.wuehn@posteo.de>
2020-01-14 21:57:40 +0100

    Updates

---
+++ precache-manifest.4749557d3ced66a44f7ec02cd39bbf81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4aafb03d0ebfa1e57043b62b1400d2a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "21bf4deed8b7393538d0",
+    "url": "/monsters-rolodex/static/css/main.67b7a62a.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21bf4deed8b7393538d0",
+    "url": "/monsters-rolodex/static/js/main.b544413c.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
